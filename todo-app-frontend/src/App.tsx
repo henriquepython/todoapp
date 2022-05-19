54054f5fb178d87e5806e9ebd207f6f194425dc0
Henriquepython <joaohbp59@gmail.com>
2022-05-19 10:09:17 -0300

    feat: create frontend project

---
+++ todo-app-frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import { AppRoutes } from './routes/index';
+function App() {
+    return (
+        <AppRoutes />
+    );
+}
+
+export default App;
---
+++ todo-app-frontend/src/shared/themes/theme.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+const fontSizes: any = [14, 20, 96];
+fontSizes.body = fontSizes[0];
+fontSizes.bodyExtraLarge = fontSizes[1];
+fontSizes.displayExtraLarge = fontSizes[2];
+
+const primary = '#2567B4';
+const secondary = '#F9B531';
+const theme = {
+    fontSizes,
+    colors: {
+        primary,
+        secondary,
+    }
+
+};
+
+interface provider {
+  children: React.ReactNode;
+}
+
+export const Theme = ( props: provider ) => {
+    const { children } = props;
+    return (
+        <ThemeProvider theme={theme}> {children} </ThemeProvider>
+    );
+};
